650993bbb044e638cff4d59f8f7d5225f2d42c77
Preston <ptkresch@gmail.com>
2016-07-11 21:52:06 -0700

    add click state

--- public/scripts/skillnode.js
+++ public/scripts/skillnode.js
@@ -1,3 +1,6 @@
+//Node bubble conversations - topics posted, can join while seeing other topics/bubbles
+//Changes to clicking nodes shoudld be handled by the nodecores
+
 var NodeBox = React.createClass({
 	loadNodesFromServer: function() {
 		$.ajax({
@@ -30,17 +33,19 @@ var NodeBox = React.createClass({
 
 var NodeList = React.createClass({
 	render: function() {
-		var nodeCores = this.props.data.map(function(node) {
-			return (
-				<NodeCore author={node.author} key={node.id} text={node.text}>
-				</NodeCore>
-			);
-		});
-		return (
-			<div className="nodeList">
-				{nodeCores}
+		return(
+			<div>
+			{this.props.data.map(function(node, i) {
+				return(
+					<div className="nodeList" key={performance.now()}>
+						<Avatar imgurl = {node.imgurl} />
+						<NodeCore author={node.author} key={node.id} id={node.id} text={node.text} skills={node.skills}>
+						</NodeCore>
+					</div>
+				);
+			})}
 			</div>
-		);
+		)
 	}
 });
 
@@ -54,10 +59,62 @@ var NodeCore = React.createClass({
 				<h4 className="nodeText">
 					{this.props.text}
 				</h4>
+				<ul className="nodeSkills">
+					{this.props.skills.map(function(skill,j) {
+						return(<li key={skill.id}>{skill.skill}</li>);
+					})}
+				</ul>
+				<Button author={this.props.author}/>
 			</div>
 		);
 	}
 });
+
+var Avatar = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<PagePic imgurl = {this.props.imgurl} />
+			</div>
+		)
+	}
+});
+
+var PagePic = React.createClass({
+	render: function() {
+		return (
+			<img src = {this.props.imgurl} />
+		);
+	}
+});
+
+class Button extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			clicked: false,
+			name: 'howdy'
+		};
+		this.handleClick = this.handleClick.bind(this);
+		this.getName = this.getName.bind(this);
+	}
+	handleClick() {
+		this.setState({clicked: !this.state.clicked});
+	}
+	getName() {
+		this.setState({name: this.props.author});
+	}
+	render() {
+		const text = this.state.clicked ? 'clicked' : 'haven\'t clicked';
+		const name = this.state.name;
+		return (
+			<div onClick={this.handleClick}>
+			You {text} this. Click to toggle.
+			<div onClick={this.getName}>Wassup {name}</div>
+			</div>
+		);
+	}
+}
 
 ReactDOM.render(
 	<NodeBox url="/api/skills" pollInterval={2000} />,
